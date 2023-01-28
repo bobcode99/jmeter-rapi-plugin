2af03d96cf54a02ff42f0430a94160df8d78b754
yb <lojobs38@gmail.com>
2023-01-28 14:58:23 +0800

    feat: add run sideex through sideexSampler

--- src/main/java/team/sideex/api/Driver.java
+++ src/main/java/team/sideex/api/Driver.java
@@ -6,15 +6,11 @@
 package team.sideex.api;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import team.sideex.api.config.Browser;
 import team.sideex.api.config.Config;
-import team.sideex.api.config.WebDriverConfig;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 
 public class Driver {
     private final Config config;
@@ -41,73 +37,6 @@ public class Driver {
         browserOptions.put("MicrosoftEdge", "ms:edgeOptions");
         return browserOptions.get(browserName);
     }
-
-    public static void main(String[] args) throws Exception {
-
-        ArrayList<String> testSuites = new ArrayList<String>();
-        testSuites.add("/Users/user/sideex-things/runner-testcase/suiteOpenWebsite.json");
-
-        Browser browser = new Browser();
-        Map<String, Object> caps = new HashMap();
-
-        // browserName: {"chrome", "firefox", "MicrosoftEdge"}
-        String browserNameValue = "MicrosoftEdge";
-        caps.put("browserName", browserNameValue);
-        HashMap<String, ArrayList<String>> browserArgs = new HashMap<>();
-
-        // setBrowserArgs: "args": ["-headless","-disable-gpu", "-window-size=1080,720"]
-        browserArgs.put("args", getBrowserArgs(browserNameValue));
-
-        // set browserOptions: "moz:firefoxOptions": {"args": ["-headless","-disable-gpu", "-window-size=1080,720"]}
-        caps.put(getBrowserOptions(browserNameValue), browserArgs);
-
-        browser.setCapability(caps);
-
-        System.out.println("browser.getCapability: " + browser.getCapability());
-
-        ArrayList<Browser> browsers = new ArrayList<Browser>();
-        browsers.add(browser);
-
-        WebDriverConfig webDriverConfig = new WebDriverConfig();
-        webDriverConfig.setBrowsers(browsers);
-        webDriverConfig.setServerUrl("http://127.0.0.1:4445");
-        ArrayList<WebDriverConfig> webDriverConfigs = new ArrayList<WebDriverConfig>();
-        webDriverConfigs.add(webDriverConfig);
-
-        Config config = new Config();
-        config.getInput().setTestSuites(testSuites);
-        config.getWebdriver().setConfigs(webDriverConfigs);
-
-        Driver driver = new Driver(
-                "/Users/user/sideex-things/runner-testcase/runner-executable-file-oneline/sideex-runner-macos-arm64", config);
-
-        JsonNode report = driver.run();
-        ObjectMapper objectMapper = new ObjectMapper();
-        System.out.println(objectMapper.writeValueAsString(report));
-    }
-
-    // private JsonNode parseReport(InputStream input) throws IOException {
-    //     String startToken = "INFO Start to send json report to api";
-    //     String endToken = "INFO End of sending json report to api";
-    //     BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-    //     String line = null;
-    //     boolean flag = false;
-    //     StringBuilder reportStr = new StringBuilder();
-    //     while ((line = reader.readLine()) != null) {
-    //         if (line.equals(endToken)) {
-    //             flag = false;
-    //         }
-    //         if (flag) {
-    //             reportStr.append(line);
-    //         }
-    //         if (line.equals(startToken)) {
-    //             flag = true;
-    //         }
-    //     }
-    //     ObjectMapper objectMapper = new ObjectMapper();
-    //     JsonNode jsonNode = objectMapper.readTree(reportStr.toString());
-    //     return jsonNode;
-    // }
 
     public JsonNode run() throws Exception {
         Proc proc = new Proc(runnerPath);
--- src/main/java/team/sideex/sampler/SideexSampler.java
+++ src/main/java/team/sideex/sampler/SideexSampler.java
@@ -1,5 +1,7 @@
 package team.sideex.sampler;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.jmeter.samplers.AbstractSampler;
 import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.SampleResult;
@@ -9,9 +11,19 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import team.sideex.api.Driver;
+import team.sideex.api.config.Browser;
+import team.sideex.api.config.Config;
+import team.sideex.api.config.WebDriverConfig;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static team.sideex.api.Driver.getBrowserArgs;
+import static team.sideex.api.Driver.getBrowserOptions;
 
 public class SideexSampler extends AbstractSampler {
 
@@ -73,47 +85,104 @@ public class SideexSampler extends AbstractSampler {
         return getPropertyAsString(BROWSER_SELECT, "Firefox");
     }
 
-    public static String getJsonString(String filePath) {
-        //JSON parser object to parse read file
-        JSONParser jsonParser = new JSONParser();
-        String stringJsonObj = null;
-        try (FileReader reader = new FileReader(filePath)) {
-            //Read JSON file
-            Object obj = jsonParser.parse(reader);
-            JSONObject myJsonObj = (JSONObject) obj;
-            stringJsonObj = myJsonObj.toString();
-            System.out.println("getJsonString: " + stringJsonObj);
+    class ResultSideex {
+        private String jsonReport;
+        private boolean successfulStatus;
+    }
 
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
-        }
-        return stringJsonObj;
+    private ResultSideex startRunSideex(String testCase, String browserName) throws Exception {
+        ArrayList<String> testSuites = new ArrayList<String>();
+        testSuites.add(testCase);
+
+        Browser browser = new Browser();
+        Map<String, Object> caps = new HashMap();
+
+        // browserName: {"chrome", "firefox", "MicrosoftEdge"}
+        String browserNameValue = browserName;
+        caps.put("browserName", browserNameValue);
+        HashMap<String, ArrayList<String>> browserArgs = new HashMap<>();
+
+        // setBrowserArgs: "args": ["-headless","-disable-gpu", "-window-size=1080,720"]
+        browserArgs.put("args", getBrowserArgs(browserNameValue));
+
+        // set browserOptions: "moz:firefoxOptions": {"args": ["-headless","-disable-gpu", "-window-size=1080,720"]}
+        caps.put(getBrowserOptions(browserNameValue), browserArgs);
+
+        browser.setCapability(caps);
+
+        System.out.println("browser.getCapability: " + browser.getCapability());
+
+        ArrayList<Browser> browsers = new ArrayList<Browser>();
+        browsers.add(browser);
+
+        WebDriverConfig webDriverConfig = new WebDriverConfig();
+        webDriverConfig.setBrowsers(browsers);
+
+        // TODO: write jmeter plugin config to send executable sideex runner's path and selenium port
+        webDriverConfig.setServerUrl("http://127.0.0.1:4445");
+        ArrayList<WebDriverConfig> webDriverConfigs = new ArrayList<WebDriverConfig>();
+        webDriverConfigs.add(webDriverConfig);
+
+        Config config = new Config();
+        config.getInput().setTestSuites(testSuites);
+        config.getWebdriver().setConfigs(webDriverConfigs);
+
+        Driver driver = new Driver(
+                "/Users/user/sideex-things/runner-testcase/runner-executable-file-oneline/sideex-runner-macos-arm64", config);
+
+        JsonNode report = driver.run();
+        String browserNameInJsonReport = report.fieldNames().next();
+//        System.out.println("report: " + report);
+//        System.out.println("report.get(browserNameInJsonReport): " + report.get(browserNameInJsonReport));
+
+        String resultSuite = report.get(browserNameInJsonReport).get(0).get("suites").get(0).get("status").asText();
+
+        ResultSideex resultSideex = new ResultSideex();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        System.out.println("resultSuite: " + resultSuite);
+
+        resultSideex.jsonReport = objectMapper.writeValueAsString(report);
+        resultSideex.successfulStatus = resultSuite.equals("success") ? true : false;
+
+        return resultSideex;
     }
 
     @Override
     public SampleResult sample(Entry entry) {
+        // Main input start here
+        //        res.sampleStart();
+        // sampleStart write in new SideexSamplerResult();
         SideexSamplerResult res = new SideexSamplerResult();
-//        res.sampleStart();
-
-        // get file contents
         String testCaseFilePath = getTestCaseFilePath();
-//        System.out.println("testCaseFilePath: " + testCaseFilePath);
-//        System.out.println(testCaseFilePath);
-        String browserName = getBrowserSelect();
+
+        // because chrome, firefox's browserName need lowercase
+        String browserName = getBrowserSelect().equals("Chrome") || getBrowserSelect().equals("Firefox") ? getBrowserSelect().toLowerCase() : getBrowserSelect();
+
+
         System.out.println("browserName: " + browserName);
+        ResultSideex resultSideex = new ResultSideex();
 
         if(!testCaseFilePath.isEmpty() && !testCaseFilePath.isBlank()) {
+
+            try {
+                resultSideex = startRunSideex(testCaseFilePath, browserName);
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+
+            }
             // here set the result of json
-            // res.setResponseMessage(resultJsonString);
-            System.out.println("write response data !");
-            String resultJsonString = getJsonString(testCaseFilePath);
-            res.setResponseData(resultJsonString, null);
+            res.setResponseMessage(resultSideex.jsonReport);
+
         }
 
-        res.setSampleLabel(getLabel());
-        res.setSuccessful(getSuccessful());
+//        res.setSampleLabel(this.getPropertyAsString(NAME));
+        res.setSampleLabel(getName());
+
+        res.setSuccessful(resultSideex.successfulStatus);
         res.setResponseCode(getResponseCode());
-        res.setResponseMessage(getResponseMessage());
+
         res.sampleEnd();
         return res;
     }
