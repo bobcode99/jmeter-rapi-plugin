64a22aa68ad552bd66d6cad3ed7d8bdcdc2e1eab
yb <lojobs38@gmail.com>
2023-02-16 22:25:12 +0800

    Move generate report code out

---
+++ src/main/java/team/sideex/report/ReportGenerator.java
@@ -0,0 +1,66 @@
+package team.sideex.report;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReportGenerator {
+    public static void generateReport(ArrayList<String> arrayList, String csvFilePath) {
+        String generateReportPath = csvFilePath.substring(0, csvFilePath.lastIndexOf('/'));
+
+        if (generateReportPath.contains("/"))
+            generateReportPath += "/";
+        else {
+            generateReportPath += "\\";
+        }
+
+        try {
+            RequestStatsReport requestStatsReport = new RequestStatsReport();
+            requestStatsReport.startGenerateReport(generateReportPath, arrayList);
+        } catch (java.text.ParseException | ParseException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    public static void checkCsvContentIsEmpty(ArrayList<String> arrayList) {
+        if(arrayList.size() == 0)  {
+            throw new RuntimeException("Please choose a .csv file that have sideex result.");
+        }
+    }
+
+    public static void checkCsvFileExist(String path) throws FileNotFoundException {
+        if(!new File(path).isFile()){
+            throw new FileNotFoundException("This .csv file not found.");
+        }
+    }
+
+    public static ArrayList<String> getSideexReportArrayList(String path) {
+        ArrayList<String> sideexReportArrayList = new ArrayList<>();
+
+        try {
+            CSVReader reader = new CSVReader(new FileReader(path));
+
+            List<String[]> records = reader.readAll();
+            for (String[] record : records) {
+                // process each record
+                Character firstCharacter = record[4].charAt(0);
+                if (firstCharacter.equals('{')) {
+                    sideexReportArrayList.add(record[4]);
+                }
+            }
+
+            checkCsvContentIsEmpty(sideexReportArrayList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CsvException e) {
+            throw new RuntimeException(e);
+        }
+        return sideexReportArrayList;
+    }
+}
--- src/main/java/team/sideex/reporter/SideexResultPanel.java
+++ src/main/java/team/sideex/reporter/SideexResultPanel.java
@@ -1,54 +1,18 @@
 package team.sideex.reporter;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
 import org.apache.jmeter.gui.util.FilePanel;
-import org.json.simple.parser.ParseException;
-import team.sideex.report.RequestStatsReport;
+import team.sideex.report.ReportGenerator;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class SideexResultPanel extends JPanel {
 
     private static final String[] ONLY_ACCEPT = {".jtl", ".csv"};
     private final FilePanel filePanel;
-    private RequestStatsReport requestStatsReport;
-
-    private void checkCsvFileExist(String path) throws FileNotFoundException {
-        if(!new File(path).isFile()){
-            throw new FileNotFoundException("This .csv file not found.");
-        }
-    }
-
-    private void checkCsvContentIsEmpty(ArrayList<String> arrayList) {
-        if(arrayList.size() == 0)  {
-            throw new RuntimeException("Please choose a .csv file that have sideex result.");
-        }
-    }
-
-    private void generateReport(ArrayList<String> arrayList, String csvFilePath) {
-        String generateReportPath = csvFilePath.substring(0, csvFilePath.lastIndexOf('/'));
-
-        if (generateReportPath.contains("/"))
-            generateReportPath += "/";
-        else {
-            generateReportPath += "\\";
-        }
-
-        try {
-            requestStatsReport = new RequestStatsReport();
-            requestStatsReport.startGenerateReport(generateReportPath, arrayList);
-        } catch (java.text.ParseException | ParseException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
+    private ReportGenerator reportGenerator;
 
     public SideexResultPanel() {
         setLayout(new BorderLayout(0, 5));
@@ -59,40 +23,16 @@ public class SideexResultPanel extends JPanel {
 
         startGenerateReportButton.addActionListener(e -> {
             String csvFilePath = filePanel.getFilename();
-
+            reportGenerator = new ReportGenerator();
             try {
-                checkCsvFileExist(csvFilePath);
+                reportGenerator.checkCsvFileExist(csvFilePath);
             } catch (FileNotFoundException ex) {
                 throw new RuntimeException(ex);
             }
-            ArrayList<String> sideexReportArrayList = getSideexReportArrayList(csvFilePath);
-            generateReport(sideexReportArrayList, csvFilePath);
+            ArrayList<String> sideexReportArrayList = reportGenerator.getSideexReportArrayList(csvFilePath);
+            reportGenerator.generateReport(sideexReportArrayList, csvFilePath);
         });
         add(filePanel, BorderLayout.NORTH);
-    }
-
-    private ArrayList<String> getSideexReportArrayList(String path) {
-        ArrayList<String> sideexReportArrayList = new ArrayList<>();
-
-        try {
-            CSVReader reader = new CSVReader(new FileReader(path));
-
-            List<String[]> records = reader.readAll();
-            for (String[] record : records) {
-                // process each record
-                Character firstCharacter = record[4].charAt(0);
-                if (firstCharacter.equals('{')) {
-                    sideexReportArrayList.add(record[4]);
-                }
-            }
-
-            checkCsvContentIsEmpty(sideexReportArrayList);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (CsvException e) {
-            throw new RuntimeException(e);
-        }
-        return sideexReportArrayList;
     }
 
 }
